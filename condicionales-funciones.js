69ffbd748bf8b32ce4bf845c44694356cdbe7efd
BrandoHormaza <brandonbohorquez0105@gmail.com>
2020-07-04 12:35:53 -0500

    trabajo en clase
    entrega pendiente

---
+++ condicionales-funciones.js
@@ -0,0 +1,12 @@
+function getRandom() {
+    return math.floor(Math.random(0) * 100);
+}
+var randomNumber = getRandom();
+
+console.log ("random number is: " + randomNumber);
+
+for(var i = 0; i < randomNumber; i++) {
+    if(  <= 50) {
+      console.log("ciclo:" + i);  
+    }
+}
